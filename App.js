b4edcf4c7fdb083d388c31116092e57219388cf3
janusanb <janusanb@gmail.com>
2021-01-22 14:02:30 -0500

    changes to hide API

--- App.js
+++ App.js
@@ -6,16 +6,6 @@ import React, { Component } from 'react'
 import firebase from 'firebase/app'
 require('firebase/auth')
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB4HjmVSqosho8LBKqEVCpVxWuIX7_qnw0", //Use enviromental variables to hide APIKey
-  authDomain: "instragram-demo.firebaseapp.com",
-  projectId: "instragram-demo",
-  storageBucket: "instragram-demo.appspot.com",
-  messagingSenderId: "460306482984",
-  appId: "1:460306482984:web:3cefa6f07448436619b21d",
-  measurementId: "G-S6CNVFEZHZ"
-};
-
 import { Provider } from 'react-redux' //is a tag that engulfs compoenents and everything in app
 import { createStore, applyMiddleware } from 'redux' //boilerplate and needs to be added to all redux-based projects
 import rootReducer from './redux/reducers'
@@ -24,7 +14,7 @@ import thunk from 'redux-thunk' // allows use of dispatch funciton, simplifying 
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
 if (firebase.apps.length === 0) {
-  firebase.initializeApp(firebaseConfig)
+    firebase.initializeApp(firebaseConfig)
 }
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -39,66 +29,87 @@ import SaveScreen from './components/main/Save'
 const Stack = createStackNavigator();
 
 export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loaded: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded: false
+        }
     }
-  }
 
-  componentDidMount() { //in specific mount moments
-    // When componenent actually mounts
-    firebase.auth().onAuthStateChanged((user) => {
-      if (!user) {
-        this.setState({
-          loggedIn: false,
-          loaded: true,
+    componentDidMount() { //in specific mount moments
+        // When componenent actually mounts
+        firebase.auth().onAuthStateChanged((user) => {
+            if (!user) {
+                this.setState({
+                    loggedIn: false,
+                    loaded: true,
+                })
+            } else {
+                this.setState({
+                    loggedIn: true,
+                    loaded: true,
+                })
+            }
         })
-      } else {
-        this.setState({
-          loggedIn: true,
-          loaded: true,
-        })
-      }
-    })
 
-  }
-  render() {
-    const { loggedIn, loaded } = this.state;
-    if (!loaded) {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-          <Text>Loading</Text>
-        </View>
-      )
     }
-    if (!loggedIn) {
-      return (
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Landing">
-            <Stack.Screen name="Landing" component={LandingScreen}></Stack.Screen>
-            <Stack.Screen name="Register" component={RegisterScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      );
+    render() {
+        const { loggedIn, loaded } = this.state;
+        if (!loaded) {
+            return ( <
+                View style = {
+                    { flex: 1, justifyContent: 'center' } } >
+                <
+                Text > Loading < /Text> <
+                /View>
+            )
+        }
+        if (!loggedIn) {
+            return ( <
+                NavigationContainer >
+                <
+                Stack.Navigator initialRouteName = "Landing" >
+                <
+                Stack.Screen name = "Landing"
+                component = { LandingScreen } > < /Stack.Screen> <
+                Stack.Screen name = "Register"
+                component = { RegisterScreen }
+                /> <
+                Stack.Screen name = "Login"
+                component = { LoginScreen }
+                /> <
+                /Stack.Navigator> <
+                /NavigationContainer>
+            );
+        }
+
+        return (
+            // this is the only way of accessing redux
+            // main screen in a stack is done so when taking a picture in instagram sends user to differnt screen
+            <
+            Provider store = { store } >
+            <
+            NavigationContainer >
+            <
+            Stack.Navigator initialRouteName = "Landing" >
+            <
+            Stack.Screen name = "Main"
+            component = { MainScreen }
+            /> <
+            Stack.Screen name = "Add"
+            component = { AddScreen }
+            navigation = { this.props.navigation }
+            /> <
+            Stack.Screen name = "Save"
+            component = { SaveScreen }
+            navigation = { this.props.navigation }
+            /> <
+            /Stack.Navigator> <
+            /NavigationContainer> <
+            /Provider>
+
+        )
     }
-
-    return (
-      // this is the only way of accessing redux
-      // main screen in a stack is done so when taking a picture in instagram sends user to differnt screen
-      <Provider store={store}>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Landing">
-            <Stack.Screen name="Main" component={MainScreen} />
-            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation} />
-            <Stack.Screen name="Save" component={SaveScreen} navigation={this.props.navigation} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </Provider>
-
-    )
-  }
 }
 
 export default App
